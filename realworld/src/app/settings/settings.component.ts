39d14982e2a1e36337acf8239b68cce6842c5da4
Tùng Trần <tranthanhtung07112000@gmail.com>
2021-11-04 16:51:45 +0700

    setting page - TUNGTT46

--- realworld/src/app/settings/settings.component.ts
+++ realworld/src/app/settings/settings.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.component.html',
-  styleUrls: ['./settings.component.scss']
+  styleUrls: ['./settings.component.scss'],
 })
 export class SettingsComponent implements OnInit {
+  name!: string;
+  email!: string;
+  password!: string;
+  urlAvatar =
+    'https://i.pinimg.com/564x/20/5a/c8/205ac833d83d23c76ccb74f591cb6000.jpg';
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  handleUpdate(ngForm: NgForm) {
+    console.log(ngForm.value);
   }
-
 }
