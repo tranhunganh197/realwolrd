9bab3bb1ad7cbfd7c2bf90c9694f1c06a788a041
anhth34 <tranhunganh197@gmail.com>
2021-11-05 16:57:33 +0700

    update anhth34

--- realworld/src/app/article/article-new/article-new.component.ts
+++ realworld/src/app/article/article-new/article-new.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-<<<<<<< HEAD
 import { NgForm } from '@angular/forms';
 import { AngularEditorConfig } from '@kolkov/angular-editor';
-=======
 import { ArticleService } from 'src/app/services/article.service';
->>>>>>> 69cadf1b6666f8dfd32c26c66e434ccf8aa4b3bf
 
 @Component({
   selector: 'app-article-new',
@@ -12,48 +9,11 @@ import { ArticleService } from 'src/app/services/article.service';
   styleUrls: ['./article-new.component.scss'],
 })
 export class ArticleNewComponent implements OnInit {
-  constructor() {}
   htmlContent!: any;
   title!: any;
   about!: any;
   tags!: any;
 
-<<<<<<< HEAD
-  config: AngularEditorConfig = {
-    editable: true,
-    spellcheck: true,
-    height: '10rem',
-    minHeight: '5rem',
-    placeholder: 'Enter text here...',
-    translate: 'no',
-    defaultParagraphSeparator: 'p',
-    defaultFontName: 'Arial',
-    toolbarHiddenButtons: [['bold']],
-    customClasses: [
-      {
-        name: 'quote',
-        class: 'quote',
-      },
-      {
-        name: 'redText',
-        class: 'redText',
-      },
-      {
-        name: 'titleText',
-        class: 'titleText',
-        tag: 'h1',
-      },
-    ],
-  };
-
-  handleAddArticle(ngForm: NgForm) {
-    console.log(2);
-    if (ngForm.submitted) {
-      ngForm.onReset();
-    }
-  }
-  ngOnInit(): void {}
-=======
   constructor(private articleService:ArticleService) { }
 
   ngOnInit(): void {
@@ -70,5 +30,4 @@ export class ArticleNewComponent implements OnInit {
   createArticle() {
     
   }
->>>>>>> 69cadf1b6666f8dfd32c26c66e434ccf8aa4b3bf
 }
