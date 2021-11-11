e39d8d62ef7995a0808527cd2f162751ae0b2458
duyvt <daovanduy125521@gmail.com>
2021-11-11 22:03:19 +0700

    'duy'

--- realworld/src/app/home/home.component.ts
+++ realworld/src/app/home/home.component.ts
@@ -21,11 +21,4 @@ export class HomeComponent implements OnInit {
     });
   }
 }
-// Xong tao bracnh
-// Tao o file front thif nho vi tri
-// truowc khi code thi pull
-// add .
-// commit 
-// pull 1 lan nua
-// xong push len nhanh e vua Tao. Tao roi thi khong can tao lai push luon len nhanh da co san
-// Xong len trang git xem co cai compare ko anh chua luwu neen chua dk
+// duy
