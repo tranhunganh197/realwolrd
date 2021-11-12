27eacfc2a4a0281af8ed2fe06db6e834cb241e65
Tùng Trần <tranthanhtung07112000@gmail.com>
2021-11-12 22:12:55 +0700

    change css tungtt46

--- realworld/src/app/profile/my-article/my-article.component.ts
+++ realworld/src/app/profile/my-article/my-article.component.ts
@@ -6,54 +6,57 @@ import { UserService } from 'src/app/services/user.service';
 @Component({
   selector: 'app-my-article',
   templateUrl: './my-article.component.html',
-  styleUrls: ['./my-article.component.scss']
+  styleUrls: ['./my-article.component.scss'],
 })
-export class MyArticleComponent implements OnInit,OnDestroy {
-
-  articles:any;
-  ob:any;
-  favoritesCount!:any;
-  isFavorite!:any;
-
+export class MyArticleComponent implements OnInit, OnDestroy {
+  articles: any;
+  ob: any;
+  favoritesCount!: any;
+  isFavorite!: any;
+  isLoading: boolean = true;
   constructor(
-    private articleService:ArticleService,
-    private userService:UserService,
-    private activatedRoute: ActivatedRoute,
-  ) { }
+    private articleService: ArticleService,
+    private userService: UserService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit() {
-    this.ob = this.userService.currentParam.subscribe((param:string) => {
-      this.userService.getProfile(param).subscribe((data:any) => {
-        this.articleService.getMyArticles(data?.profile?.username).subscribe((data:any) => {
-          this.articles = data?.articles;
-        })
-      })
-    })
+    this.ob = this.userService.currentParam.subscribe((param: string) => {
+      this.userService.getProfile(param).subscribe((data: any) => {
+        this.articleService
+          .getMyArticles(data?.profile?.username)
+          .subscribe((data: any) => {
+            this.articles = data?.articles;
+            if (this.articles !== undefined && this.articles.length > 0) {
+              this.isLoading = false;
+            }
+          });
+      });
+    });
   }
 
-  toggleLike(isFavoried:boolean,slug:string) {
+  toggleLike(isFavoried: boolean, slug: string) {
     if (isFavoried) {
-      this.articleService.unFavorite(slug).subscribe((data:any)  => {
-        this.articles.map((article:any,index:any) => {
+      this.articleService.unFavorite(slug).subscribe((data: any) => {
+        this.articles.map((article: any, index: any) => {
           if (article?.slug === data?.article?.slug) {
             this.articles[index] = data?.article;
           }
         });
-      })
+      });
     } else {
-      console.log(isFavoried)
-      this.articleService.favorite(slug).subscribe((data:any) => {
-        this.articles.map((article:any,index:any) => {
+      console.log(isFavoried);
+      this.articleService.favorite(slug).subscribe((data: any) => {
+        this.articles.map((article: any, index: any) => {
           if (article?.slug === data?.article?.slug) {
             this.articles[index] = data?.article;
           }
         });
-      })
+      });
     }
   }
 
   ngOnDestroy() {
     this.ob.unsubscribe();
   }
-
 }
