b4e26843907080da3b6b5c9be287595154aa57c0
Tùng Trần <tranthanhtung07112000@gmail.com>
2021-11-15 00:16:52 +0700

    buttton scroll

--- realworld/src/app/app.component.ts
+++ realworld/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'realworld';
+  hideBtn: boolean = false;
+  @HostListener('window:scroll', [])
+  onScroll(): void {
+    // if (window.innerHeight + window.scrollY >= document.body.offsetHeight) {
+    //   console.log('2');
+    // } else if (
+    //   window.innerHeight + window.scrollY <
+    //   document.body.offsetHeight
+    // ) {
+    //   console.log('1');
+    // }
+    if (window.innerHeight > window.scrollY) {
+      this.hideBtn = false;
+    } else {
+      this.hideBtn = true;
+    }
+  }
+
+  handleScroll() {
+    window.scrollTo(0, 0);
+  }
 }
